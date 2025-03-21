47e9f769284420e36a5e0f63a67bbfa0f3faa512
Diego Alexandre <ddsa.1988@outlook.com>
2025-03-21 01:38:39 -0300

    commit

---
+++ Basics/Chapter002/src/Main.java
@@ -0,0 +1,7 @@
+import firstApp.FirstApp;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        FirstApp.userMain();
+    }
+}
---
+++ Basics/Chapter002/src/firstApp/FirstApp.java
@@ -0,0 +1,7 @@
+package firstApp;
+
+public class FirstApp {
+    public static void userMain() {
+        System.out.println("This is a simple Java program.");
+    }
+}
