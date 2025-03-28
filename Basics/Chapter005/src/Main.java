3f6312a1abccdcba1d2b810fb7546cbe9ffc9b7e
Diego Alexandre <d.alexandre@acxautomation.com>
2025-03-28 15:18:57 -0300

    commit

--- Basics/Chapter005/src/Main.java
+++ Basics/Chapter005/src/Main.java
@@ -1,7 +1,7 @@
-import examples.jumpStatements.*;
+import exercises.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        ReturnDemo.userMain();
+        Exercise001.userMain();
     }
 }
---
+++ Basics/Chapter005/src/exercises/Exercise001.java
@@ -0,0 +1,7 @@
+package exercises;
+
+public class Exercise001 {
+    public static void userMain() {
+
+    }
+}
