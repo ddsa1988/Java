ad31479adc3a5231fa6d61dff00742ebacc3e557
Diego Alexandre <ddsa.1988@outlook.com>
2025-03-30 22:18:05 -0300

    commit

--- Basics/Chapter006/src/examples/BoxDemo1.java
+++ Basics/Chapter006/src/examples/BoxDemo1.java
@@ -3,6 +3,7 @@ package examples;
 public class BoxDemo1 {
     public static void userMain() {
         Box mybox = new Box();
+
         mybox.width = 10;
         mybox.height = 20;
         mybox.depth = 15;
