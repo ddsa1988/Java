7c3c2e7bb146ec94df093c4efa275ddfe1e587ff
Diego Alexandre <ddsa.1988@outlook.com>
2025-04-08 20:37:59 -0300

    commit

--- Basics/Chapter008/src/Main.java
+++ Basics/Chapter008/src/Main.java
@@ -1,7 +1,7 @@
-import abstractClass.*;
+import typeInference.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        TestDemo2.userMain();
+        TestDemo.userMain();
     }
 }
