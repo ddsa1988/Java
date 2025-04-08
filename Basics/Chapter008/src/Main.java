5ca63482622f372f0b2e1bcae3de3f95d4259eeb
Diego Alexandre <ddsa.1988@outlook.com>
2025-04-07 23:17:06 -0300

    commit

--- Basics/Chapter008/src/Main.java
+++ Basics/Chapter008/src/Main.java
@@ -2,6 +2,6 @@ import dynamicMethodDispatch.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        TestDemo1.userMain();
+        TestDemo2.userMain();
     }
 }
---
+++ Basics/Chapter008/src/abstractClass/TestDemo1.java
@@ -0,0 +1,7 @@
+package abstractClass;
+
+public class TestDemo1 {
+    public static void userMain() {
+
+    }
+}
