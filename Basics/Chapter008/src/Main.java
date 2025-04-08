a7410f374d7cbad5f08dacf942e0073c8d698e97
Diego Alexandre <d.alexandre@acxautomation.com>
2025-04-08 14:43:39 -0300

    commit

--- Basics/Chapter008/src/Main.java
+++ Basics/Chapter008/src/Main.java
@@ -1,4 +1,4 @@
-import dynamicMethodDispatch.*;
+import abstractClass.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
---
+++ Basics/Chapter008/src/finalKeyword/TestDemo1.java
@@ -0,0 +1,26 @@
+package finalKeyword;
+
+public class TestDemo1 {
+    // To disallow a method from being overridden, specify final as a modifier at
+    // the start of its declaration.
+    public static void userMain() {
+        ClassB obj = new ClassB();
+
+        System.out.println(obj);
+    }
+}
+
+class ClassA {
+    public final void callMe() {
+        System.out.println("Method callMe.");
+    }
+}
+
+class ClassB extends ClassA {
+    /*
+     * @Override
+     * public final void callMe() {
+     * System.out.println("Method callMe.");
+     * }
+     */
+}
