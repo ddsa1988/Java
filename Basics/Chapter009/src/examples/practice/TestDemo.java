0d92d8950395767780bc9787d0870a38dd1ab8c0
Diego Alexandre <d.alexandre@acxautomation.com>
2025-04-11 06:48:13 -0300

    commit

--- Basics/Chapter009/src/examples/practice/TestDemo.java
+++ Basics/Chapter009/src/examples/practice/TestDemo.java
@@ -4,25 +4,30 @@ import java.util.Arrays;
 import java.util.List;
 
 public class TestDemo {
+    @SuppressWarnings({ "rawtypes", "unchecked" })
     public static void userMain() {
         var stackGeneric = new Stack<Float>();
         var stackStr = new StackString(5);
         var stackInteger = new StackInteger(10);
 
-        stackGeneric.push(18.5f);
-        stackGeneric.push(71.58f);
+        IStack stackRef = stackGeneric;
 
-        stackStr.push("Diego");
-        stackStr.push("Amanda");
+        stackRef.push(18.5f);
+        stackRef.push(71.58f);
 
-        stackInteger.push(100);
-        stackInteger.push(200);
+        stackRef = stackStr;
 
-        @SuppressWarnings("rawtypes")
+        stackRef.push("Diego");
+        stackRef.push("Amanda");
+
+        stackRef = stackInteger;
+
+        stackRef.push(100);
+        stackRef.push(200);
+
         List<IStack> stacks = Arrays.asList(stackGeneric, stackStr, stackInteger);
 
-        for (@SuppressWarnings("rawtypes")
-        IStack stack : stacks) {
+        for (IStack stack : stacks) {
             System.out.println(stack);
         }
     }
